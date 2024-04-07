b50130309e11383ac7876be91b5e221ebc5d008d
Zeke Binion <zeke@binion.io>
2024-04-07 15:23:51 -0400

    ci: set port for Railway

--- main.go
+++ main.go
@@ -1,17 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	if _, isSet := os.LookupEnv("PORT"); !isSet {
+		os.Setenv("PORT", "8000")
+	}
+
+	addr := fmt.Sprintf("0.0.0.0:%s", os.Getenv("PORT"))
 	mux := gin.Default()
 
 	mux.GET("/", hamdleRootGet)
 
-	mux.Run()
+	mux.Run(addr)
 }
 
 func hamdleRootGet(ctx *gin.Context) {
